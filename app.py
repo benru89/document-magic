9392753b530df0c7e86e69015c07fbe3db3001ee
ruben <ruben.fernandez@addsecure.com>
2024-10-04 17:59:19 +0200

    st secrets

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ from langchain import LLMChain, PromptTemplate
 import fitz  # PyMuPDF
 
 # Load the API key from .env file
-load_dotenv()
-api_key = st.secrets["OPENAI_API_KEY"]
+api_key = st.secrets["general"]["OPENAI_API_KEY"]
 
 # Use ChatOpenAI instead of OpenAI for chat models like gpt-3.5-turbo
 llm = ChatOpenAI(model="gpt-3.5-turbo", api_key=api_key)
